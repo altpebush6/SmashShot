4ad66e3ae1fc742cf36cbccb95e50486680ffa09
ebu <93250409+altpebush6@users.noreply.github.com>
2024-10-18 23:25:22 +0300

    Initialize project

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BallManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private TextMeshProUGUI ballCount;
+    [SerializeField] private Transform playerTF;
+
+    [SerializeField] private float cooldownTime = 1f;
+
+    private bool instantiatable = true;
+    private int ballRight = 5;
+
+    void Update()
+    {
+        if(Input.GetButton("InstantiateBall") && instantiatable && ballRight > 0)
+        {
+            ballRight--;
+
+            StartCoroutine(BounceCooldown());
+
+            Vector2 position = new Vector2(playerTF.transform.position.x, playerTF.transform.position.y + 5f);
+
+            Instantiate(ballPrefab, position, Quaternion.identity); 
+
+            ballCount.text = ballRight.ToString();
+        }
+    }
+
+    IEnumerator BounceCooldown()
+    {
+        instantiatable = false;
+        yield return new WaitForSeconds(cooldownTime);
+        instantiatable = true;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer; 
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        var horizontal = Input.GetAxis("Horizontal");        
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if(horizontal > 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+
+        animator.SetFloat("Speed",Mathf.Abs(rb.velocity.x));
+    }
+
+    void Jump()
+    {
+        if(Input.GetButton("Vertical") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        animator.SetBool("isGrounded", isGrounded);
+    }
+}
